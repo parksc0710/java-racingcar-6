b609d05efece5da706c743a0e406c4e5bc63322b
parks <parks@210.101.73.115>
2023-10-27 00:03:43 +0900

    init

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,26 @@
 package racingcar;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import racingcar.objects.RacingCar;
+import racingcar.utils.PrintUtil;
+
 public class Application {
+	
+	// 공통 변수
+	public static List<RacingCar> carList = new ArrayList<RacingCar>();
+	public static int racingLoop = 0;
+	public static final int movingGauge = 4;
+	
+	// main 메소드
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+		PrintUtil.startGame();
     }
+    
+    // 예외 발생 시 throw
+    public static void throwException(String logTxt) {
+    	throw new IllegalArgumentException(logTxt);
+    }
+    
 }
